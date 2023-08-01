0374fea8010e568be125dfe509050b87a6a8cd6f
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-08-01 23:27:36 +0900

    front landing
    front landing

---
+++ front-landing/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier', 'import', 'unused-imports'],
+    extends: [
+        'airbnb',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        camelcase: 'off',
+        'linebreak-style': 0,
+        'import/prefer-default-export': 0,
+        'prettier/prettier': 0,
+        'import/extensions': 0,
+        'no-use-before-define': 0,
+        'import/no-unresolved': 0,
+        'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+        'no-shadow': 0,
+        'react/prop-types': 0,
+        'react/jsx-filename-extension': [2, {extensions: ['.js', '.jsx', '.ts', '.tsx']}],
+        'jsx-a11y/no-noninteractive-element-interactions': 0,
+        'react/function-component-definition': [2, {namedComponents: 'arrow-function'}],
+        "react/require-default-props": "warn",
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'builtin',
+                        position: 'before',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['react'],
+            },
+        ],
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            {vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_'},
+        ],
+    },
+};
