6fc35424878b93df84d2e379ba3a2de49bec83e3
Hangeulkim <ckswls89@naver.com>
2023-09-23 16:33:35 +0900

    Revert "랜딩"
    This reverts commit 8f56cc1758797838a87e3864102d68177878908e.

--- front-landing/src/App.tsx
+++ front-landing/src/App.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import {ApolloProvider} from "@apollo/client/react";
-import SignupForm from './components/SignUp';
-import LoginForm from './components/Login';
-import PhoneBookApp from './components/Phone';
+import { AppRouter } from './routes/AppRouter';
 
-const client = new ApolloClient({
-    uri: 'https://moviethree.synology.me/back/graphql',
-    cache: new InMemoryCache()
-});
-const App: React.FC = () => {
-    return (
-        <ApolloProvider client={client}>
-        <Router>
-            <Routes>
-                <Route path="/signup" element={<SignupForm />} />
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/phonebook" element={<PhoneBookApp />} />
-            </Routes>
-        </Router>
-        </ApolloProvider>
-    );
+const App = () => {
+    return <AppRouter />;
 };
 
 export default App;
