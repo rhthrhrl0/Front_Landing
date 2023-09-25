342944a6282e561f47b8645cca44f5d50fefa95b
BOOK-1V2KOUUPE2\dmltn <dmltn3426@naver.com>
2023-09-25 17:27:04 +0900

    Revert "Feature/test (#1)"
    This reverts commit 6cf11c9c0f63e43fed94f96779e78abd83ce7566.

--- front-landing/src/App.tsx
+++ front-landing/src/App.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import {ApolloClient, InMemoryCache,ApolloProvider} from '@apollo/client'
-import {AppRouter} from './routes/AppRouter';
+import { AppRouter } from './routes/AppRouter';
 
 const App = () => {
-    const client = new ApolloClient({
-        uri: 'https://moviethree.synology.me/back/graphql',
-        cache: new InMemoryCache()
-    })
-    return (
-        <ApolloProvider client={client}>
-            <AppRouter/>
-        </ApolloProvider>
-    );
-
+    return <AppRouter />;
 };
 
 export default App;
